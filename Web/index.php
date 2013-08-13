3f3dd58f21a6407f629c69423f78a968353bd17a
Ben Gotow <bengotow@gmail.com>
2013-08-13 15:54:38 -0700

    Updated the CSS a bit to center the grid and make the items slightly smaller so its 3x wide, and added new "mine" mode for viewing your own drawings only

--- Web/index.php
+++ Web/index.php
@@ -26,21 +26,39 @@ if (!(isset($_GET['pagenum'])))
 else
 	$pagenum = $_GET['pagenum'];
 
-$num_threads = 6; // The number of threads to be shown per page
-
-$data = $db->query("SELECT * FROM thread") or die(mysql_error());
-$num_rows = $data->num_rows;
-$last_page_num = ceil($num_rows/$num_threads); // This is the number of the last page.
-
 if ($pagenum < 1) $pagenum = 1;
-if ($pagenum > $last_page_num) 	$pagenum = $last_page_num;
-	
+
+$num_per_page = 30; // The number of threads to be shown per page
+$currentpage = $_SERVER["PHP_SELF"]."?46px_user_id=".$_GET["46px_user_id"]."&mine=".$_GET["mine"];
+
 // Sets the range of the query
-$max = 'LIMIT ' .($pagenum - 1)* $num_threads . ',' . $num_threads;
+$max = 'LIMIT ' .($pagenum - 1)* $num_per_page . ',' . $num_per_page;
+
+if ($_GET['mine'] == true) {
+	$mine = true;
+	$count_query = "SELECT COUNT(*) as count FROM post WHERE user_id = ".mysql_escape_string($_GET['46px_user_id'])." GROUP BY post.thread_id";
+	$thread_query = "SELECT thread_id as id FROM post WHERE user_id = ".mysql_escape_string($_GET['46px_user_id'])." GROUP BY post.thread_id ORDER BY timestamp DESC " . $max;
+} else {
+	$mine = false;
+	$count_query = "SELECT COUNT(*) as count FROM thread";
+	$thread_query = "SELECT * FROM thread ORDER BY timestamp DESC " . $max;
+}
+
+$query = $db->query($count_query) or die(mysql_error());
+$query_row = $query->fetch_assoc();
+$last_page_num = ceil($query_row['count'] / $num_per_page);
+
+
+if ($mine) {
+	echo '<div class="note"><div>Showing drawings you posted or replied to. <a href="index.php?45px_user_id='.$_GET["46px_user_id"].'">Show all drawings instead.</a></div></div>';
+} else if ($pagenum == 1){
+	echo '<div class="note"><div><strong>Welcome to 46px!</strong> These are pixel art drawings that others have created. Start your own tiny drawing, or tap one of the drawings below to draw your own version!</div></div>';
+}
 
 echo "<ul id='threads'>\n";
-if ($result = $db->query("SELECT * FROM thread ORDER BY timestamp Desc " . $max)) {
-    
+
+$result = $db->query($thread_query);
+if ($result) {
     for ($i=0; $i < $result->num_rows; $i++) {
 		$thread = $result->fetch_assoc();
 		$post_result = $db->query("SELECT *, users.id as user_id FROM post INNER JOIN users ON post.user_id = users.id WHERE thread_id= ". $thread['id'] ." ORDER BY timestamp LIMIT 1");
@@ -50,7 +68,7 @@ if ($result = $db->query("SELECT * FROM thread ORDER BY timestamp Desc " . $max)
 		
 		<li class='post'>
 			<a href="thread.php?thread=<?=$thread['id'] . "&46px_user_id=" . $_GET['46px_user_id']?>">			
-				<canvas style="width:230px; height:230px;" id='canvas<?=$i ?>'></canvas>
+				<canvas id='canvas<?=$i ?>'></canvas>
 			</a>
           
            <script>
@@ -60,12 +78,12 @@ if ($result = $db->query("SELECT * FROM thread ORDER BY timestamp Desc " . $max)
            i.src = "<?= str_replace('http://www.46px.com', '', $post['image_url']) ?>";
            </script>
 
-		<div class="caption" style="width:230px; background-color:rgba(255,255,255,0.5); border:0px; padding:5px; clear:left; margin-top:10px; word-wrap: break-word;">
-			<img style="float:left; padding-right:5px;" src="http://graph.facebook.com/<?=$post["user_id"]?>/picture">
-			<div style="float:right; width:170px;">
+		<div class="caption">
+			<img src="http://graph.facebook.com/<?=$post["user_id"]?>/picture">
+			<div id="postheader">
 				<b><?=$post["first_name"]. " ". $post["last_name"] ?></b><br>
-				<?=$post['caption']?><br>
-				<span style="font-size:0.9em; color:gray;"><?= date('D, M jS \a\t g:iA', strtotime($post['timestamp'])) ?></span><br>
+				<?=stripcslashes($post['caption']) ?><br>
+				<span id="timestamp"><?= date('D, M jS \a\t g:iA', strtotime($post['timestamp'])) ?></span><br>
 			</div>
 			<div style="Clear:both;"></div>
 		</div>
@@ -74,38 +92,44 @@ if ($result = $db->query("SELECT * FROM thread ORDER BY timestamp Desc " . $max)
         <?
         $curthread = $i + 1;
         
-        if ($curthread % 2 == 0)
+        if ($curthread % 3 == 0)
         	echo '<div style="Clear:both;"></div>';
     }
     $result->close();
 
 } else {
-	echo "Didn't access DB\n";
+	echo "No results to display";
 }
 
 echo "</ul>\n";
 ?>
-<div style="width: 100%; text-align:center;">
+<div id="page_controls">
+<div style="float:left;">
 <?
 if ($pagenum > 1){
-	echo " <a class = 'button' style='width=50' href='{$_SERVER['PHP_SELF']}?pagenum=1'> < First</a> ";
+	echo " <a class='button' style='width=50' href='{$currentpage}&pagenum=1'> < First</a> ";
 	echo "      ";
 	$previous = $pagenum-1;
-	echo " <a class = 'button' style='width=50' href='{$_SERVER['PHP_SELF']}?pagenum=$previous'> < Previous</a> ";
+	echo " <a class='button' style='width=50' href='{$currentpage}&pagenum=$previous'> < Previous</a> ";
  } 
-
+?>
+</div>
+<div style="float:right;">
+<?
  //This does the same as above, only checking if we are on the last page, 
  //and then generating the Next and Last links
  if ($pagenum != $last_page_num){
 	$next = $pagenum+1;
-	echo " <a class='button' style='width=50' href='{$_SERVER['PHP_SELF']}?pagenum=$next'>Next ></a> ";
+	echo " <a class='button' style='width=50' href='{$currentpage}&pagenum=$next'>Next ></a> ";
 	echo "      ";
-	echo " <a class='button' style='width=50' href='{$_SERVER['PHP_SELF']}?pagenum=$last_page_num'>Last ></a> ";
+	echo " <a class='button' style='width=50' href='{$currentpage}&pagenum=$last_page_num'>Last ></a> ";
  } 
 
 $db->close();
 ?>
 </div>
+</div>
+<div style="clear:both;"></div>
 </div>
 </body>
 </html>
